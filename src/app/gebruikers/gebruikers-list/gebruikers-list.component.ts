595400b483377325301d59147d3588a434620459
Pieter Pauwels <r0796064@student.thomasmore.be>
2022-01-20 13:07:53 +0100

    Replaced user with gebruiker

---
+++ src/app/gebruikers/gebruikers-list/gebruikers-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gebruikers-list',
+  templateUrl: './gebruikers-list.component.html',
+  styleUrls: ['./gebruikers-list.component.scss']
+})
+export class GebruikersListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
