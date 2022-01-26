8c8d8df56ee3d20dd9d3e52d7aaace6d3bff6e2b
Pieter Pauwels <r0796064@student.thomasmore.be>
2022-01-26 09:10:46 +0100

    Add only bearer token to http request to API

--- src/app/security/token.interceptor.ts
+++ src/app/security/token.interceptor.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -11,23 +12,28 @@ export class TokenInterceptor implements HttpInterceptor {
     constructor(private authService: AuthService, private router: Router) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // All HTTP requests are going to go through this method
-        const token = this.authService.getToken();
-        if (token) {
-            req = req.clone({
-                setHeaders: {
-                    Authorization: "Bearer " + token
-                }
-            });
-        }
+        // Add Bearer token only to routes that contain the URL of the api
+        if (req.url.includes(environment.API_URI)) {
+            const token = this.authService.getToken();
+            if (token) {
+                req = req.clone({
+                    setHeaders: {
+                        Authorization: "Bearer " + token
+                    }
+                });
+            }
 
-        return next.handle(req).pipe(
-            catchError(err => {
-                if (err.status === 401) {
-                    this.router.navigate(['login']);
-                }
-                return throwError(err);
-            })
-        );
+            return next.handle(req).pipe(
+                catchError(err => {
+                    if (err.status === 401) {
+                        this.router.navigate(['login']);
+                    }
+                    return throwError(err);
+                })
+            );
+        }
+        else {
+            return next.handle(req);
+        }
     }
 }
