97629f7de84758589a9f06d505f3b4427ff2619d
Julian Koppens <r0802564@student.thomasmore.be>
2022-01-28 09:36:27 +0100

    route zodat Kris alle alarmwaarden kan bekijken

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { GebruikersListComponent } from './gebruikers/gebruikers-list/gebruikers-list.component';
 import { WeerstationListComponent } from './weerstations/weerstation-list/weerstation-list.component';
 import { OrganisatieListComponent } from './organisaties/organisatie-list/organisatie-list.component';
+import { WeerstationsAlarmSchakelwaardesListComponent } from '../organisatiebeheerder/weerstations-organisatie/weerstations-alarm-schakelwaardes-list/weerstations-alarm-schakelwaardes-list.component';
 
 const routes: Routes = [
     { path: 'gebruikers', component: GebruikersListComponent },
     { path: 'weerstations', component: WeerstationListComponent },
     { path: 'organisaties', component: OrganisatieListComponent }, // TODO: crud maken
+    { path: 'waardes/:id', component: WeerstationsAlarmSchakelwaardesListComponent },
+
 ];
 
 @NgModule({
