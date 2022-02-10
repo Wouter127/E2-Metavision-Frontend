f1ef400a25647462d1a0d2e0400abb1b391f9520
Wouter127 <r0785520@student.thomasmore.be>
2022-02-10 22:16:03 +0100

    location terug knop en side-menu fixed, opmaak, loading

--- src/app/non-auth/weerstation-dashboard-location/weerstation-dashboard-location.component.ts
+++ src/app/non-auth/weerstation-dashboard-location/weerstation-dashboard-location.component.ts
@@ -12,12 +12,11 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./weerstation-dashboard-location.component.scss']
 })
 
-export class WeerstationDashboardLocationComponent implements OnInit, AfterViewInit {
+export class WeerstationDashboardLocationComponent implements OnInit {
   @Input() title!: string;
 
   map: any;
-  
-  showModal: boolean = false;
+  loading: boolean = true;
 
   icon = {
     icon: L.icon({
@@ -30,18 +29,11 @@ export class WeerstationDashboardLocationComponent implements OnInit, AfterViewI
 
   coordinatesArray: number[] = [];
 
-  weerstation!: Weerstation;
   meting!: any;
   getLaatsteMeting$: Subscription = new Subscription();
   routeParams$: Subscription = new Subscription();
 
-  constructor(private route: ActivatedRoute, private authWeerstationService: WeerstationService) {
-    
-  }
-
-  ngAfterViewInit(): void {
-    
-  }
+  constructor(private route: ActivatedRoute, private authWeerstationService: WeerstationService) {}
 
   ngOnInit(): void {
     this.getCoordinates();
@@ -51,35 +43,27 @@ export class WeerstationDashboardLocationComponent implements OnInit, AfterViewI
     this.routeParams$ = this.route.params.subscribe(
       params => {
         this.getLaatsteMeting$= this.authWeerstationService.getLaatsteMeting(params['id']).subscribe(
-          (result: any) => {
+          (result: any) => {            
             this.meting = result;
-            this.initMap();            
+            this.loading = false; 
+            this.initMap(result);
+                      
           }
         );
       }
     );
   }
   
-  private initMap(): void {
+  private initMap(result: any): void {
     //const map = L.map("map").setView([this.coordinatesArray], 15);
-    const map = L.map("map").setView([this.meting.gla, this.meting.glo], 15);
+    const map = L.map("map").setView([result.gla, result.glo], 15);
     //const map = L.map("map").setView([50, 4.98917], 15);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    const marker = L.marker([this.meting.gla, this.meting.glo], this.icon).addTo(map);
-    marker.bindPopup("" + this.weerstation.naam);    
-    
+    const marker = L.marker([result.gla, result.glo], this.icon).addTo(map);
+    marker.bindPopup("" + result.weerstation.naam);    
   }
-
-  openModal(){
-    this.showModal = true;
-  }
-
-  closeModal(){
-    this.showModal = false;
-  }
-
 }
