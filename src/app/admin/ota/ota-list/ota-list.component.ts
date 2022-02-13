28ef51b4a0c26e3d60fc6c0a7ba7c8e9238e9436
Pieter Pauwels <r0796064@student.thomasmore.be>
2022-02-14 00:09:20 +0100

    Renamed ota plannen component

---
+++ src/app/admin/ota/ota-list/ota-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ota-list',
+  templateUrl: './ota-list.component.html',
+  styleUrls: ['./ota-list.component.scss']
+})
+export class OtaListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
